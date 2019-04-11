d8ae83ee0764d32feceb391fe7c9cfaeb62ff1b9
Quinten Clause <quinten.clause@studioemma.eu>
2019-04-11 22:34:21 +0200

    Add index.html with html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,53 @@
 
 const webpack = require('webpack');
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const config = require('./package');
 
-module.exports = {
+module.exports = (env, argv) => {
 
-    entry: './src/index.js',
+    const devMode = argv.mode !== 'production';
 
-    output: {
-        path: path.resolve(__dirname, 'pub/build'),
-        publicPath: '/build/',
-        filename: 'project.bundle.js'
-    },
+    let webpackConfig = {
 
-    module: {
-        rules: [
-          {
-            test: [ /\.vert$/, /\.frag$/ ],
-            use: 'raw-loader'
-          }
-        ]
-    },
+        entry: './src/index.js',
 
-    plugins: [
-        new webpack.DefinePlugin({
-            'CANVAS_RENDERER': JSON.stringify(true),
-            'WEBGL_RENDERER': JSON.stringify(true)
-        })
-    ],
+        output: {
+            path: path.resolve(__dirname, 'pub'),
+            publicPath: '',
+            filename: 'project.bundle.js'
+        },
 
-    devServer: {
-        contentBase: "./pub",
-        host: "0.0.0.0",
-        port: 8020
+        module: {
+            rules: [{
+                test: [ /\.vert$/, /\.frag$/ ],
+                use: 'raw-loader'
+            }]
+        },
+
+        plugins: [
+            new HtmlWebpackPlugin({
+                ...config,
+                template: 'src/index.html'
+            }),
+            new webpack.DefinePlugin({
+                'CANVAS_RENDERER': JSON.stringify(true),
+                'WEBGL_RENDERER': JSON.stringify(true)
+            })
+        ],
+
+        devServer: {
+            contentBase: "./pub",
+            host: "0.0.0.0",
+            port: 8020
+        }
     }
+
+    if (!devMode) {
+        // some more production stuff
+        // ...
+    }
+
+    return webpackConfig;
 
 };
